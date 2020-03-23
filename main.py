bdf23193867ad0bf4da70bf4550d82921ca59293
Franci2004 <effeesse04@gmail.com>
2020-03-23 11:17:23 +0100

    modifica del file py

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("ciao", nome)
 age = int(input("quanti anni hai? "))
 n = int(input("ora ti faccio una magia: dimmi un numero "))
 print(n*2)
+a = int(input("inserire un altro numero "))
+if a < 14:
+	a += 1
+else:
+	a -= 1
+print(a)
